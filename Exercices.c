ebd1db90cfb7132cdfcaa3947b9bf0fce865f473
eloise <eloise.ansoult@student.junia.com>
2025-04-11 17:43:00 +0200

    etape 2

--- Exercices.c
+++ Exercices.c
@@ -48,10 +48,31 @@ void trierParDatePeremption(Medicament tab[], int n) {
     }
 }
 
-
-
 void afficherMedicament(Medicament med) {
     printf("%s (Expire le %d )\n", med.nom, med.peremption);
+}
+
+int rechercheDichotomique(Medicament tab[], int n, char nomRecherche[]) {
+    int debut = 0;
+    int fin = n - 1;
+    int mil;
+    int trouve = 0;
+
+    while (!trouve && debut <= fin) {
+        mil = (debut + fin) / 2;
+        int cmp = strcmp(nomRecherche, tab[mil].nom);
+
+        if (cmp == 0) {
+            trouve = 1;
+            return mil;
+        } else if (cmp > 0) {
+            debut = mil + 1;
+        } else {
+            fin = mil - 1;
+        }
+    }
+
+    return -1;
 }
 
 int main() {
@@ -72,6 +93,18 @@ int main() {
     printf("\n=== Medicaments tries par date de peremption ===\n");
     for (i = 0; i < n; i++) {
         afficherMedicament(tab[i]);
+    }
+
+    char nomRecherche[50];
+    printf("Entrez le nom du médicament à rechercher : ");
+    scanf("%s", nomRecherche);
+
+    int pos = rechercheDichotomique(tab, n, nomRecherche);
+
+    if (pos != -1) {
+        printf("Le medicament \"%s\" est trouve au rang %d.\n", nomRecherche, pos);
+    } else {
+        printf("Le medicament \"%s\" n'est pas dans le tableau.\n", nomRecherche);
     }
 
     return 0;
